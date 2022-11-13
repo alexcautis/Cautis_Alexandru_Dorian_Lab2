fe8e61f5c3c5c76b1460c0a1943743347d56bd40
Cunt 2.0 <Cunt 2.0@DESKTOP-RDHO5U1>
2022-11-13 13:46:32 +0200

    Add project files.

---
+++ Data/Cautis_Alexandru_Dorian_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cautis_Alexandru_Dorian_Lab2.Models;
+
+namespace Cautis_Alexandru_Dorian_Lab2.Data
+{
+    public class Cautis_Alexandru_Dorian_Lab2Context : DbContext
+    {
+        public Cautis_Alexandru_Dorian_Lab2Context (DbContextOptions<Cautis_Alexandru_Dorian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cautis_Alexandru_Dorian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cautis_Alexandru_Dorian_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cautis_Alexandru_Dorian_Lab2.Data;
+using Cautis_Alexandru_Dorian_Lab2.Models;
+
+namespace Cautis_Alexandru_Dorian_Lab2.Pages.Publisher
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Cautis_Alexandru_Dorian_Lab2.Data.Cautis_Alexandru_Dorian_Lab2Context _context;
+
+        public IndexModel(Cautis_Alexandru_Dorian_Lab2.Data.Cautis_Alexandru_Dorian_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            {
+                List<Book> books = await _context.Book
+                                .Include(b => b.Publisher)
+                                .ToListAsync();
+                Book = books;
+            }
+
+            if (_context.Publisher != null)
+            {
+                List<Models.Publisher> publishers = await _context.Publisher.ToListAsync();
+                Publisher = (IList<Publisher>)publishers;
+            }
+        }
+    }
+}
